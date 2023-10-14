578be5f777aff36e0681b882bdd764b52bad5398
dmitrii <DmitryXox02@yandex.ru>
2023-10-14 20:48:11 +0300

    Refactor cors

--- src/main/java/com/example/prehack/config/CORSFilter.java
+++ src/main/java/com/example/prehack/config/CORSFilter.java
@@ -10,7 +10,6 @@ public class CORSFilter implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
                 .allowedOriginPatterns("http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
