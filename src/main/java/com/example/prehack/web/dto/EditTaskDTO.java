907344a1ece79188a411163348b793c681ff21db
Oladushek <gyrf921@gmail.com>
2023-10-15 06:17:29 +0300

    deploy editTask x1

---
+++ src/main/java/com/example/prehack/web/dto/EditTaskDTO.java
@@ -0,0 +1,34 @@
+package com.example.prehack.web.dto;
+
+import com.example.prehack.model.enumformodel.Priority;
+import com.example.prehack.model.enumformodel.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EditTaskDTO {
+
+    private String name;
+
+    private String description;
+
+    private Priority priority;
+
+    private Status status;
+
+    private LocalDate dataStart;
+
+    private LocalDate dataFinish;
+
+    private Long projectId;
+
+    private String userEmail;
+
+}
